8f146f19007a350319bcde0ad8e2d7d354576373
John Weaver <john.weaver.astro@gmail.com>
2019-03-11 22:20:55 -0700

    added command line interface

---
+++ bin/tractor_phot.py
@@ -0,0 +1,9 @@
+# This is a very quick launch script
+
+import sys
+sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
+sys.path.insert(0, os.path.join(os.getcwd(), 'config'))
+from core import interface
+
+
+interface.tractor(sys.argv[1])
