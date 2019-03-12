421611d97b3a1cb1084dfba182e5f053f199aac2
John Weaver <john.weaver.astro@gmail.com>
2019-03-11 22:22:28 -0700

    minor fix

--- bin/tractor_phot.py
+++ bin/tractor_phot.py
@@ -1,6 +1,7 @@
 # This is a very quick launch script
 
 import sys
+import os
 sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
 sys.path.insert(0, os.path.join(os.getcwd(), 'config'))
 from core import interface
