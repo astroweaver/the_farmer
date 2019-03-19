672568ed192e3b85a06b3642aa085dda5e44a6c3
John Weaver <john.weaver.astro@gmail.com>
2019-03-18 17:40:26 -0700

    remove verbose debug

--- src/core/blob.py
+++ src/core/blob.py
@@ -111,8 +111,6 @@ class Blob(Subimage):
             tweight = weight.copy()
             tweight[mask] = 0
 
-
-            print(f'I THINK THE PSF IS {psf}')
             if psf == -99:
                 psfmodel = NCircularGaussianPSF([conf.PSF_SIGMA,], [1,])
                 print('WARNING - Adopting FAKE PSF model!')
