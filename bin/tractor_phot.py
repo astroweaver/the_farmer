a1732d32eb60c0e827bf985cefcf20f500c8efa6
John Weaver <john.weaver.astro@gmail.com>
2019-04-04 01:38:30 +0200

    added conf import to command line method

--- bin/tractor_phot.py
+++ bin/tractor_phot.py
@@ -5,6 +5,7 @@ import os
 sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
 sys.path.insert(0, os.path.join(os.getcwd(), 'config'))
 from core import interface
+import config as conf
 
 # if len(sys.argv) == 2:
 #     interface.tractor(int(sys.argv[1]))
