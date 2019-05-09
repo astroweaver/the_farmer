cb798fa8a10f66d861a96db6a74429f096e66446
John Weaver <john.weaver.astro@gmail.com>
2019-05-09 22:50:48 +0200

    Updated ap phot

--- src/core/interface.py
+++ src/core/interface.py
@@ -287,7 +287,7 @@ def runblob(blob_id, blobs, detection=None, catalog=None, plotting=False):
             for img_type in ('image', 'model', 'residual'):
                 for band in detblob.bands:
                     try:
-                        detblob.aperture_phot(band, img_type)
+                        detblob.aperture_phot(band, img_type, sub_background=CONF.SUBTRACT_BACKGROUND)
                     except:
                         if conf.VERBOSE: print(f'Aperture photmetry FAILED for {band} {img_type}. Likely a bad blob.')
         if conf.DO_SEXPHOT:
@@ -368,7 +368,7 @@ def runblob(blob_id, blobs, detection=None, catalog=None, plotting=False):
             for img_type in ('image', 'model', 'residual'):
                 for band in fblob.bands:
                     try:
-                        fblob.aperture_phot(band, img_type)
+                        fblob.aperture_phot(band, img_type, sub_background=True)
                     except:
                         if conf.VERBOSE: print(f'Aperture photmetry FAILED for {band} {img_type}. Likely a bad blob.')
         if conf.DO_SEXPHOT:
