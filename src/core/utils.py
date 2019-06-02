ff051ea6f7920acf7b42ef7a99804a939e183c1f
John Weaver <john.weaver.astro@gmail.com>
2019-06-02 19:32:05 +0200

    typo

--- src/core/utils.py
+++ src/core/utils.py
@@ -291,7 +291,7 @@ def plot_detblob(blob, fig=None, ax=None, level=0, sublevel=0, final_opt=False, 
                 minx = resmin
             if resmax > maxx:
                 maxx = resmax
-            if not init:s
+            if not init:
                 ax[nrow,4].text(0.02, 0.9 - 0.1*i, r'$\chi^{2}$'+f'={blob.chisq[i, level, sublevel]:2.2f} | BIC={blob.bic[i, level, sublevel]:2.2f}',
                          color=colors[i], transform=ax[nrow,4].transAxes)
         ax[nrow,5].set_xlim(minx, maxx)
