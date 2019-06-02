126345f55dfd42550e1b9957f5d8c432655ca8b2
John Weaver <john.weaver.astro@gmail.com>
2019-06-02 21:46:03 +0200

    Try starting with zero flux models

--- src/core/blob.py
+++ src/core/blob.py
@@ -159,7 +159,7 @@ class Blob(Subimage):
                 position = self.tr_catalogs[i,0,0].getPosition()
             else:
                 position = PixPos(src['x'], src['y'])
-            flux = Fluxes(**dict(zip(self.bands, 1*np.ones(self.n_bands))))
+            flux = Fluxes(**dict(zip(self.bands, np.zeros(self.n_bands))))
 
             #shape = GalaxyShape(src['a'], src['b'] / src['a'], src['theta'])
             shape = EllipseESoft.fromRAbPhi(src['a'], src['b'] / src['a'], -0.5*src['theta'])
