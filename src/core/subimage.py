4f070844df3dda0114f63f8a34b39a7cd041d9ca
John Weaver <john.weaver.astro@gmail.com>
2019-08-20 13:27:06 +0200

    fixed some annoying verbose statements

--- src/core/subimage.py
+++ src/core/subimage.py
@@ -276,8 +276,9 @@ class Subimage():
         # Convert band to index for arrays
         # TODO: Handle more than one band at a time
         if band in self.bands:
-            print(conf.BANDS, band)
-            return np.arange(len(conf.BANDS))[np.array(conf.BANDS) == band][0]
+            idx = np.arange(len(conf.BANDS))[np.array(conf.BANDS) == band][0]
+            if conf.VERBOSE2: print(f'subimage._band2idx :: {band} returns idx={idx}'}
+            return idx
         else:
             raise ValueError(f"{band} is not a valid band.")
 
