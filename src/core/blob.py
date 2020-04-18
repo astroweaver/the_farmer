Misc fixes. Auto scaling for fphot_psig in test

src/core/blob.py
@@ -238,6 +238,7 @@ class Blob(Subimage):
                     return False
                 hdul = fits.open(path_prffile)
                 from scipy.ndimage.interpolation import rotate
+                # img = rotate(hdul[0].data, 270)
                 img = hdul[0].data
                 # img = 1E-31 * np.ones_like(img)
                 # img[50:-50, 50:-50] = hdul[0].data[50:-50, 50:-50]
@@ -446,11 +447,30 @@ class Blob(Subimage):
             #     x_orig[i] = src.pos[0]
             #     y_orig[i] = src.pos[1]
 
+
+            if conf.PLOT > 1:
+                plot_iterblob(self, tr, iteration=0, bands=self.bands)
+
             for i in range(conf.TRACTOR_MAXSTEPS):
+
+                # if i == 0:
+                #     pos = PixPos(13, 30)
+                #     pos.addGaussianPrior('x', 13, 2)
+                #     pos.addGaussianPrior('y', 30, 2)
+                #     tr.addSource(PointSource(pos, Fluxes(**dict(zip(self.bands, 0.1 * np.ones(len(self.bands)))))))
+
                 if conf.TRY_OPTIMIZATION:
                     try:
+                        [print(m.getPosition()) for m in tr.getCatalog()]
                         dlnp, X, alpha, var = tr.optimize(shared_params=self.shared_params, damp=conf.DAMPING, variance=True, priors=conf.USE_POSITION_PRIOR)
                         self.logger.debug(f'    {i+1}) dlnp = {dlnp}')
+                        [print(m.getPosition()) for m in tr.getCatalog()]
+                        [print(m.getThawedParams()) for m in tr.getCatalog()]
+                        [print(m.getFrozenParams()) for m in tr.getCatalog()]
+
+                        
+
+
                         if i == 0:
                             dlnp_init = dlnp
                     except:
@@ -483,7 +503,7 @@ class Blob(Subimage):
                 #     return False
 
                 if conf.PLOT > 1:
-                    plot_iterblob(self, tr, iteration=i, bands=self.bands)
+                    plot_iterblob(self, tr, iteration=i+1, bands=self.bands)
 
                 if dlnp < conf.TRACTOR_CONTHRESH:
                     self.logger.info(f'Blob #{self.blob_id} converged in {i+1} steps ({dlnp_init:2.2f} --> {dlnp:2.2f}) ({time.time() - tstart:3.3f}s)')
@@ -492,7 +512,8 @@ class Blob(Subimage):
 
                 if conf.CORRAL_SOURCES:
                     # check if any sources have left their segment
-                    cat = tr.getCatalog()
+                    cat = tr.getCatalog()[:len(self.bcatalog)]
+                    trip = False
                     for idx, src in enumerate(cat):
                         sid = self.bcatalog['source_id'][idx]
                         xp, yp = src.pos[0], src.pos[1]
@@ -502,6 +523,7 @@ class Blob(Subimage):
 
                         if (xp > maxx) | (xp < 0) | (yp < 0) | (yp > maxy):
                             self.logger.warning(f'Source {sid} has escaped the blob!')
+                            trip = True
 
                             # gpriors = src.getLogPrior()
                             # print('Log(Prior):', gpriors)
@@ -531,8 +553,8 @@ class Blob(Subimage):
                         #     # gpriors = src.getLogPrior()
                         #     # print('Log(Prior):', gpriors)
 
-
-                    tr.setCatalog(cat)
+                    if trip:
+                        tr.setCatalog(cat)
 
                 
         if var is None:
@@ -664,7 +686,9 @@ class Blob(Subimage):
                     self.logger.debug(f'Source #{src["source_id"]} with {self.model_catalog[i].name} has rchisq={self.rchisq[i, self._level, self._sublevel]:3.3f} | bic={self.bic[i, self._level, self._sublevel]:3.3f}')
                     self.logger.debug(f'     with {len(self.bands)} bands, {m_param} parameters, {n_data} points in total --> NDOF = {ndof}')
                     for k, band in enumerate(self.bands):
+                        self.logger.debug(f'               Positions: {self.bands[k]}={self.model_catalog[i].getPosition()}') 
                         self.logger.debug(f'               Fluxes: {self.bands[k]}={self.model_catalog[i].getBrightness().getFlux(self.bands[k]):3.3f}+/-{np.sqrt(self.variance[i].brightness.getParams()[k]):3.3f}') 
+
                     if self.model_catalog[i].name not in ('PointSource', 'SimpleGalaxy'):
                         if self.model_catalog[i].name == 'FixedCompositeGalaxy':
                             self.logger.debug(f'               {self.model_catalog[i].shapeExp}')
@@ -1412,10 +1436,12 @@ class Blob(Subimage):
                     self.bcatalog[row][f'RA_{band}'] = skyc[0]
                     self.bcatalog[row][f'DEC_{band}'] = skyc[1]
 
+            pos = 0000
             mag, magerr = self.bcatalog[row]['MAG_'+band], self.bcatalog[row]['MAGERR_'+band]
             flux, fluxerr = self.bcatalog[row]['FLUX_'+band], self.bcatalog[row]['FLUXERR_'+band]
             rawflux, rawfluxerr = self.bcatalog[row]['RAWFLUX_'+band], self.bcatalog[row]['RAWFLUXERR_'+band]
             chisq, bic = self.bcatalog[row]['CHISQ_'+band], self.bcatalog[row]['BIC_'+band]
+            self.logger.info(f'    Position({band}):     {pos}')
             self.logger.info(f'    Raw Flux({band}):     {rawflux:3.3f} +/- {rawfluxerr:3.3f}')
             self.logger.info(f'    Flux({band}):         {flux:3.3f} +/- {fluxerr:3.3f} uJy')               
             self.logger.info(f'    Mag({band}):          {mag:3.3f} +/- {magerr:3.3f} AB')

src/core/brick.py
@@ -260,11 +260,11 @@ class Brick(Subimage):
         self.model_images = np.zeros(shape=(self.n_bands, np.shape(self.images[0])[0], np.shape(self.images[0])[1]))
         self.chisq_images = np.zeros(shape=(self.n_bands, np.shape(self.images[0])[0], np.shape(self.images[0])[1]))
 
-        if modeling:
-            sbands = [s[len(conf.MODELING_NICKNAME)+1:] for s in self.bands]
-        else:
-            sbands = self.bands
-
+        # if modeling:
+        #     sbands = [s[len(conf.MODELING_NICKNAME)+1:] for s in self.bands]
+        # else:
+        #     sbands = self.bands
+        sbands = self.bands
         self.bands = np.array(self.bands) # The bands attribute is being modfified somewhere to make it BACK into a list. Why? Dunno. Just don't change this.
 
         # Figure out which bands are to be run with which setup.
@@ -597,8 +597,8 @@ class Brick(Subimage):
 
         for i, (psf, band) in enumerate(zip(self.psfmodels[idx], self.bands[idx])):
 
-            if modeling:
-                band = band[len(conf.MODELING_NICKNAME)+1:]
+            # if modeling:
+            #     band = band[len(conf.MODELING_NICKNAME)+1:]
 
             remove_background_psf = False
             if band in conf.RMBACK_PSF:

src/core/interface.py
@@ -1242,7 +1242,7 @@ def force_photometry(brick_id, band=None, source_id=None, blob_id=None, insert=T
     if (not unfix_bandwise_positions) | (len(fband) == 1):
         force_models(brick_id=brick_id, band=band, source_id=source_id, blob_id=blob_id, insert=insert, source_only=source_only)
     else:
-        if not conf.FREEZE_FORCED_POSITION:
+        if conf.FREEZE_FORCED_POSITION:
             logger.warning('Setting FREEZE_FORCED_POSITION to False!')
             conf.FREEZE_FORCED_POSITION = False
        
@@ -1640,9 +1640,12 @@ def make_model_image(brick_id, band, catalog=None, use_single_band_run=False, mo
 def make_residual_image(brick_id, band, catalog=None, use_single_band_run=False, modeling=False):
     # USE BAND w/ MODELING NICKNAME FOR MODELING RESULTS!
 
-    if band.startswith(conf.MODELING_NICKNAME):
+    if band.startswith(conf.MODELING_NICKNAME) | ((modeling==True) & (band != conf.MODELING_NICKNAME)):
         nickname = conf.MULTIBAND_NICKNAME
-        sband = band[len(conf.MODELING_NICKNAME)+1:]
+        if band.startswith(conf.MODELING_NICKNAME):
+            sband = band[len(conf.MODELING_NICKNAME)+1:]
+        else:
+            sband = band
         modeling=True
     elif band == conf.MODELING_NICKNAME:
         nickname = conf.MODELING_NICKNAME
@@ -1834,13 +1837,14 @@ def models_from_catalog(catalog, fblob):
                 max_img = np.nanmax(img * src_seg)                                              # TODO THESE ARENT ALWAYS THE SAME SHAPE!
                 max_psf = np.nanmax(fblob.psfimg[band])
                 qflux[j] = max_img / max_psf
+                # print(max_img, max_psf, qflux[j])
             flux = Fluxes(**dict(zip(fblob.bands, qflux)))
 
         except:
             try:
                 original_zpt = conf.MODELING_ZPT
                 logger.info(f'Converting fluxes from zerpoint {original_zpt} to {target_zpt}')
-                flux_conv = src[f'RAWFLUX_{best_band}'] * 10 ** (0.4 * (target_zpt - original_zpt))
+                qflux = src[f'RAWFLUX_{best_band}'] * 10 ** (0.4 * (target_zpt - original_zpt))
             except:
                 # IF I TRIED MULTIBAND MODELING, THEN I STILL NEED AN INITIAL FLUX. START WITH 0 idx!
                 init_band = f'{conf.MODELING_NICKNAME}_{conf.INIT_FLUX_BAND}'
@@ -1848,9 +1852,9 @@ def models_from_catalog(catalog, fblob):
                     conf.INIT_FLUX_BAND = fblob.bands[0]
                 logger.warning(f'Coming from multiband model, so using flux from {init_band}')
                 original_zpt = fblob._band2idx(conf.INIT_FLUX_BAND)
-                flux_conv = src[f'RAWFLUX_{init_band}'] * 10 ** (0.4 * (target_zpt - original_zpt))
+                qflux = src[f'RAWFLUX_{init_band}'] * 10 ** (0.4 * (target_zpt - original_zpt))
                 
-            flux = Fluxes(**dict(zip(band, flux_conv)))
+            flux = Fluxes(**dict(zip(band, qflux)))
 
         # Check if valid source
         if not src[f'VALID_SOURCE_{best_band}']:
@@ -1859,9 +1863,20 @@ def models_from_catalog(catalog, fblob):
             continue
 
         if not conf.FREEZE_FORCED_POSITION & conf.USE_POSITION_PRIOR:
-            logger.info(f'Setting position prior. X = {inpos[0]:2.2f}+/-{conf.FORCE_POSITION_PRIOR_SIG}; Y = {inpos[1]:2.2f}+/-{conf.FORCE_POSITION_PRIOR_SIG}')
-            position.addGaussianPrior('x', inpos[0], conf.FORCE_POSITION_PRIOR_SIG)
-            position.addGaussianPrior('y', inpos[1], conf.FORCE_POSITION_PRIOR_SIG)
+            ffps = conf.FORCE_POSITION_PRIOR_SIG
+            if conf.FORCE_POSITION_PRIOR_SIG in ('auto', 'AUTO'):
+                snr = np.nanmedian(qflux / fblob.backgrounds[:,1])
+                snr_thresh = 5
+                pos_sig_under = 0.1
+                if snr < snr_thresh:
+                    ffps = pos_sig_under
+                else:
+                    ffps = snr/(snr_thresh/pos_sig_under) 
+                print(snr, ffps)
+                # conf.FORCE_POSITION_PRIOR_SIG = 1 - np.exp(-0.5*src[f'CHISQ_{conf.MODELING_NICKNAME}_{conf.INIT_FLUX_BAND}'])
+            logger.info(f'Setting position prior. X = {inpos[0]:2.2f}+/-{ffps}; Y = {inpos[1]:2.2f}+/-{ffps}')
+            position.addGaussianPrior('x', inpos[0], ffps)
+            position.addGaussianPrior('y', inpos[1], ffps)
 
         #shape = GalaxyShape(src['REFF'], 1./src['AB'], src['theta'])
         if src[f'SOLMODEL_{best_band}'] not in ('PointSource', 'SimpleGalaxy'):