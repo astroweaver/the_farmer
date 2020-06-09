cc40f859d304e6dc396739d8359a38da7d322695
John Weaver <john.weaver.astro@gmail.com>
2020-06-09 18:23:05 +0200

    qf

--- src/core/interface.py
+++ src/core/interface.py
@@ -307,7 +307,7 @@ def make_bricks(image_type=conf.MULTIBAND_NICKNAME, band=None, brick_id=None, in
 
             # Assume we can overwrite files unless insertion is explicit
             # First image w/o insertion will make new file anyways
-            overwrite = True
+            overwrite = conf.OVERWRITE
             if insert | (i > 0):
                 overwrite = False
 
