96de1285e64bf41739327168ee6c8a1319dfbff8
John Weaver <john.weaver.astro@gmail.com>
2020-08-17 19:46:18 +0200

    qf

--- src/core/visualization.py
+++ src/core/visualization.py
@@ -250,9 +250,13 @@ def plot_srcprofile(blob, src, sid, bands=None):
 
         is_resolved = False
         if src.name not in ('PointSource', 'SimpleGalaxy'):
-            rband = conf.MODELING_NICKNAME + '_' + band # Does this always work?! No.
             is_resolved = True
-            reff, reff_err = np.exp(bsrc[f'REFF_{rband}'][0])*conf.PIXEL_SCALE, np.exp(bsrc[f'REFF_{rband}'][0])*bsrc[f'REFF_ERR_{rband}'][0]*2.303*conf.PIXEL_SCALE
+            try:
+                rband = conf.MODELING_NICKNAME + '_' + band # Does this always work?! No.
+                reff, reff_err = np.exp(bsrc[f'REFF_{rband}'][0])*conf.PIXEL_SCALE, np.exp(bsrc[f'REFF_{rband}'][0])*bsrc[f'REFF_ERR_{rband}'][0]*2.303*conf.PIXEL_SCALE
+            except:
+                rband = band # Does this always work?! No.
+                reff, reff_err = np.exp(bsrc[f'REFF_{rband}'][0])*conf.PIXEL_SCALE, np.exp(bsrc[f'REFF_{rband}'][0])*bsrc[f'REFF_ERR_{rband}'][0]*2.303*conf.PIXEL_SCALE
             ab, ab_err = bsrc[f'AB_{rband}'][0], bsrc[f'AB_ERR_{rband}'][0]
             if ab == -99.0:
                 ab = -99
